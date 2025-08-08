4703a5b833849f58467c02f6159b2c375f6c6a71
patga <publicpatonai@gmail.com>
2025-08-08 13:10:28 +0200

    removed compiled attribute as it is not needed

--- src/PkiFabric.Core/Helpers/RegularExpressions.cs
+++ src/PkiFabric.Core/Helpers/RegularExpressions.cs
@@ -14,31 +14,26 @@ internal static partial class RegularExpressions
 
     [GeneratedRegex(
         @"^S-(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])-(?:0|[1-9][0-9]{0,15})(?:-(?:0|[1-9][0-9]{0,9})){1,255}$",
-        RegexOptions.CultureInvariant | RegexOptions.Compiled,
-        ShortTimeout)]
+        RegexOptions.CultureInvariant, ShortTimeout)]
     internal static partial Regex Sid();
 
     [GeneratedRegex(
         @"^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$",
-        RegexOptions.CultureInvariant | RegexOptions.Compiled,
-        matchTimeoutMilliseconds: ShortTimeout)]
+        RegexOptions.CultureInvariant, matchTimeoutMilliseconds: ShortTimeout)]
     internal static partial Regex Email();
 
     [GeneratedRegex(
         @"^(https?:\/\/)?(www\.)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}(\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)?$",
-        RegexOptions.CultureInvariant | RegexOptions.Compiled,
-        matchTimeoutMilliseconds: ShortTimeout)]
+        RegexOptions.CultureInvariant, matchTimeoutMilliseconds: ShortTimeout)]
     internal static partial Regex Url();
 
     [GeneratedRegex(
-            @"^((25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})(\.|$)){4}$",
-            RegexOptions.CultureInvariant | RegexOptions.Compiled,
-            matchTimeoutMilliseconds: ShortTimeout)]
+        @"^((25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})(\.|$)){4}$",
+        RegexOptions.CultureInvariant, matchTimeoutMilliseconds: ShortTimeout)]
     internal static partial Regex Ipv4();
 
     [GeneratedRegex(
-            @"^(([0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}|(::1)|::)$",
-            RegexOptions.CultureInvariant | RegexOptions.Compiled,
-            matchTimeoutMilliseconds: ShortTimeout)]
+        @"^(([0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}|(::1)|::)$",
+        RegexOptions.CultureInvariant, matchTimeoutMilliseconds: ShortTimeout)]
     internal static partial Regex Ipv6();
 }
