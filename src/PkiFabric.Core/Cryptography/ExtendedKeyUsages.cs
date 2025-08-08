d48f73b063a4cb098e1496a8184744680759c8e0
patga <publicpatonai@gmail.com>
2025-08-08 12:04:28 +0200

    added ExtendedKeyUsages

---
+++ src/PkiFabric.Core/Cryptography/ExtendedKeyUsages.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) PATGABOR. All rights reserved.
+// Licensed under the Apache License 2.0 license.
+
+namespace PkiFabric.Core.Cryptography;
+
+/// <summary>
+/// Represents common Extended Key Usage (EKU) purposes for X.509 certificates.
+/// These values correspond to standard EKU object identifiers (OIDs) used to define
+/// the intended purpose of a certificate's public key.
+/// </summary>
+public enum ExtendedKeyUsages
+{
+    /// <summary>
+    /// Any extended key usage is acceptable (OID: 2.5.29.37.0).
+    /// </summary>
+    Any,
+
+    /// <summary>
+    /// Server authentication for TLS/SSL connections (OID: 1.3.6.1.5.5.7.3.1).
+    /// </summary>
+    ServerAuth,
+
+    /// <summary>
+    /// Client authentication for TLS/SSL connections (OID: 1.3.6.1.5.5.7.3.2).
+    /// </summary>
+    ClientAuth,
+
+    /// <summary>
+    /// Code signing to verify the integrity and origin of software (OID: 1.3.6.1.5.5.7.3.3).
+    /// </summary>
+    CodeSigning,
+
+    /// <summary>
+    /// Secure email protection, including signing and encryption (OID: 1.3.6.1.5.5.7.3.4).
+    /// </summary>
+    EmailProtection,
+
+    /// <summary>
+    /// Trusted timestamping to prove data existed at a certain time (OID: 1.3.6.1.5.5.7.3.8).
+    /// </summary>
+    TimeStamping,
+}
---
+++ src/PkiFabric.Core/Helpers/ExtendedKeyUsageAdapter.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) PATGABOR. All rights reserved.
+// Licensed under the Apache License 2.0 license.
+
+using System.Collections.Immutable;
+
+using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Asn1.X509;
+
+using PkiFabric.Core.Cryptography;
+
+namespace PkiFabric.Core.Helpers;
+
+/// <summary>
+/// Adapts an <see cref="ImmutableHashSet{T}"/> of <see cref="ExtendedKeyUsages"/> 
+/// into a BouncyCastle <see cref="Asn1EncodableVector"/> containing the 
+/// corresponding <see cref="KeyPurposeID"/> entries for use in X.509 extended key usage extensions.
+/// </summary>
+public sealed class ExtendedKeyUsageAdapter : IAdapter<ImmutableHashSet<ExtendedKeyUsages>, Asn1EncodableVector>
+{
+    /// <summary>
+    /// Gets a default instance of <see cref="ExtendedKeyUsageAdapter"/>.
+    /// </summary>
+    public static ExtendedKeyUsageAdapter Default { get; } = new();
+
+    /// <inheritdoc/>
+    public Asn1EncodableVector Adapt(ImmutableHashSet<ExtendedKeyUsages> source)
+    {
+        Asn1EncodableVector extendedKeyUsages = [];
+
+        foreach (ExtendedKeyUsages usage in source)
+        {
+            switch (usage)
+            {
+                case ExtendedKeyUsages.Any:
+                    extendedKeyUsages.Add(KeyPurposeID.AnyExtendedKeyUsage);
+                    break;
+
+                case ExtendedKeyUsages.ServerAuth:
+                    extendedKeyUsages.Add(KeyPurposeID.id_kp_serverAuth);
+                    break;
+
+                case ExtendedKeyUsages.ClientAuth:
+                    extendedKeyUsages.Add(KeyPurposeID.id_kp_clientAuth);
+                    break;
+
+                case ExtendedKeyUsages.CodeSigning:
+                    extendedKeyUsages.Add(KeyPurposeID.id_kp_codeSigning);
+                    break;
+
+                case ExtendedKeyUsages.EmailProtection:
+                    extendedKeyUsages.Add(KeyPurposeID.id_kp_emailProtection);
+                    break;
+
+                case ExtendedKeyUsages.TimeStamping:
+                    extendedKeyUsages.Add(KeyPurposeID.id_kp_timeStamping);
+                    break;
+
+                default:
+                    throw new InvalidOperationException($"Unsupported Extended Key Usage {usage}.");
+            }
+        }
+        return extendedKeyUsages;
+    }
+}
